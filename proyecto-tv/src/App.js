beeeb34dd619452856c3262ba42baa6190aa0f55
ocralo <e_lopez1996@hotmail.com>
2019-03-28 14:34:04 -0500

    se añade el spinner de cargando en app.js y el estilo en app.css

--- proyecto-tv/src/App.js
+++ proyecto-tv/src/App.js
@@ -3,6 +3,12 @@ import Loadable from "react-loadable";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 
+const loading = () => (
+  <div className="rel-cargando">
+    <div id="loading" />
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
