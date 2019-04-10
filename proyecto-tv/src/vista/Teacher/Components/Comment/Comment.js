3a231e1178890e8bca344c516f818af9a29475d6
Lab Informatica <lab.informatica@uao.edu.co>
2019-04-10 17:19:21 -0500

    Se creo el componete comentarios

---
+++ proyecto-tv/src/vista/Teacher/Components/Comment/Comment.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Comment.css';
+
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { imagen: "", nombre: "", comentario: "", tiempo: "" }
+  }
+
+  render() {
+    return (
+      <div className="container mb-3">
+        <div className="row no-gutters">
+          <div className="col-md-4 card-jdp ml-3 mt-2">
+            <img src={this.props.imagen} className="card-img"/>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body p-2">
+              <h5 className="card-title mb-1 ">{this.props.nombre}</h5>
+              <p className="card-text mb-1">{this.props.comentario}</p>
+              <p className="card-text"><small className="text-muted">{this.props.tiempo}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Comment;
