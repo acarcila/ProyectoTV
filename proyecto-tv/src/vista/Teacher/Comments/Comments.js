c8b6aef156347a12c2411c78a5dace2980e84efa
Momer <cagg40@hotmail.com>
2019-04-11 03:25:35 -0500

    Se creo el componente para agregar comentario

--- proyecto-tv/src/vista/Teacher/Comments/Comments.js
+++ proyecto-tv/src/vista/Teacher/Comments/Comments.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "./Comments.css";
 import Header3 from "../Components/Header3/Header3"
 import Comment from "../Components/Comments-Teacher/Comments-Teacher"
+import Question from "../Components/Question-Modal/Question"
 import { Player, ControlBar } from "video-react";
 import "video-react/dist/video-react.css";
 
 const sources = {
     sintelTrailer: "./assets/video/Que_es_Multimedia.mp4",
     bunnyTrailer: "./assets/video/Curso-React-Introduccion.mp4"
-  };
+};
 
 export default class Login extends Component {
     constructor() {
@@ -37,11 +38,15 @@ export default class Login extends Component {
                     <div className="card" id="Comments-main-divs">
                         <div id="Comments-title">
                             <h4>Comentarios(4)</h4>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                Comentario
+                            </button>
+                            <Question/>
                         </div>
                         <div id="Comments-div">
-                        <Comment imagen="/assets/img/foto.png" nombre="Rodrogo" comentario="¿Qué es UX?" tiempo="Min 1:20"/>
-                        <Comment imagen="/assets/img/foto.png" nombre="Camilox" comentario="¿Usabilidad?" tiempo="Min 2:50"/>
-                        <Comment imagen="/assets/img/foto.png" nombre="Possillo" comentario="¿De que clase de prueba habla?" tiempo="Min 10:00"/>
+                            <Comment imagen="/assets/img/foto.png" nombre="Rodrogo" comentario="¿Qué es UX?" tiempo="Min 1:20" />
+                            <Comment imagen="/assets/img/foto.png" nombre="Camilox" comentario="¿Usabilidad?" tiempo="Min 2:50" />
+                            <Comment imagen="/assets/img/foto.png" nombre="Possillo" comentario="¿De que clase de prueba habla?" tiempo="Min 10:00" />
                         </div>
                     </div>
                 </section>
