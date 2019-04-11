5cce9b73a147ef9af7411d2343a6d34fdc0d52a2
ocralo <e_lopez1996@hotmail.com>
2019-04-11 00:03:12 -0500

    se agrega el cambio de vista a home y tambien al video interactivo con audio

--- proyecto-tv/src/App.js
+++ proyecto-tv/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Loadable from "react-loadable";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 const loading = () => (
   <div className="rel-cargando">
@@ -13,13 +13,40 @@ const Login = Loadable({
   loader: () => import("./vista/Login/Login"),
   loading
 });
+const Home = Loadable({
+  loader: () => import("./vista/Home/Home"),
+  loading
+});
+
+
+const InteractiveSound = Loadable({
+  loader: () => import("./vista/InteractiveSound/InteractiveSound"),
+  loading
+});
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
         <Switch>
-          <Route exact path="/" name="Login - IMUAO" component={Login} />
+          <Route
+            exact
+            path="/"
+            name="Login - YOURLEARN"
+            component={Login}
+          />
+          <Route
+            exact
+            path="/Home"
+            name="Home - YOURLEARN"
+            component={Home}
+          />
+          <Route
+            exact
+            path="/InteractiveSound"
+            name="Video Home - YOURLEARN"
+            component={InteractiveSound}
+          />
         </Switch>
       </HashRouter>
     );
